dcdfef3c17059f5df5b9eb598bf66208e4168fc6
Evan <allainevan1@gmail.com>
2025-06-06 16:51:52 +0200

    Adjust import

--- client/app/modifier/[id]/page.js
+++ client/app/modifier/[id]/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useRouter, useParams } from 'next/navigation';
-import api from '@/lib/api';
+import api from '../../../lib/api';
 
 export default function ModifierAnnonce() {
   const router = useRouter();
